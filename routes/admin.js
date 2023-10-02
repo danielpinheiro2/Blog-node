5f9b83bf5d19d0a7ccf36e778f551a84cf2f16a7
danielpinheiro2 <d.hapinheiro@gmail.com>
2023-10-02 16:46:28 -0300

    02/10

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
+
 router.get('/', (req,res) => {
+    res.render("admin/index")
+})
+
+router.get('/a', (req,res) => {
     res.send("Página principal do painel ADM")
 })
 
