a346a709c430209e359ca2faf0c83f193cf17adb
danielpinheiro2 <d.hapinheiro@gmail.com>
2023-10-04 17:22:18 -0300

    Validation

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ npm install --proxy=http://danielpinheiro:@sesa2.proxy.dcpr.es.gov.br:1892 body-
 npm install --proxy=http://danielpinheiro:@sesa2.proxy.dcpr.es.gov.br:1892 --save mongoose
 
 npm install --proxy=http://danielpinheiro:@sesa2.proxy.dcpr.es.gov.br:1892 -g nodemon
-33 5:18
+npm install --proxy=http://danielpinheiro:@sesa2.proxy.dcpr.es.gov.br:1892 --save express-session
+npm install --proxy=http://danielpinheiro:@sesa2.proxy.dcpr.es.gov.br:1892 --save connect-flash
--- app.js
+++ app.js
@@ -6,7 +6,24 @@ const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
 const mongoose = require("mongoose")
+const session =require("express-session")
+const flash = require("connect-flash")
+
 // Configurações
+  // sessao
+    app.use(session({
+      secret: "cursodenode",
+      resave: true,
+      saveUninitialized: true
+    }))
+    app.use(flash())
+  // middleware
+    app.use((req,res,next)=>{
+      res.locals.success_msg = req.flash("success_msg")
+      res.locals.error_msg = req.flash("error_msg")
+      next()
+    })
+
     //body parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -16,12 +33,10 @@ const mongoose = require("mongoose")
     //app.set('view engine', 'handlebars');
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
-    // mongoose
-
 
     // configurando o mongoose
     mongoose.Promise = global.Promise;
-    mongoose.connect("mongodb://127.0.0.1/blogapp")
+    mongoose.connect("mongodb://127.0.0.1/api")
       .then(() => {
         console.log("Servidor conectado!")
       })
@@ -31,6 +46,10 @@ const mongoose = require("mongoose")
     // public
         app.use(express.static(path.join(__dirname, "public")))
 
+        app.use((req, res, next) => {
+          console.log("Middleware")
+          next()
+        })
 
 // Rotas
     app.use('/admin', admin)
