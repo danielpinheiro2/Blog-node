a82c1facd4cefe6ec6bbbb8c4bfd8b6a826362a7
danielpinheiro2 <d.hapinheiro@gmail.com>
2023-11-05 23:54:46 -0300

    user logout

--- routes/usuario.js
+++ routes/usuario.js
@@ -80,4 +80,14 @@ router.post("/login", (req, res, next) => {
     })(req, res ,next)
 })
 
+router.get('/logout', (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success_msg', 'Deslogado com sucesso!');
+        res.redirect('/');
+    });
+});
+
 module.exports = router
